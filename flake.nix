1e548b4cd9a74d66c56b60738a8fa1062f70b46e
Arthur Zapparoli <arthurgeek@users.noreply.github.com>
2026-04-05 20:34:07 -0300

    chore: init zig project with nix flakes
    Signed-off-by: Arthur Zapparoli <arthurgeek@users.noreply.github.com>

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "ziglings";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    zig-overlay.url = "github:mitchellh/zig-overlay";
+    zls = {
+      url = "github:zigtools/zls";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.zig-overlay.follows = "zig-overlay";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, zig-overlay, zls }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        zig = zig-overlay.packages.${system}.master;
+      in
+      {
+        # `nix develop`
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            zig
+            zls.packages.${system}.default
+          ];
+        };
+      }
+    );
+}
